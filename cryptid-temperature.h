010571237986c7b5511a8addae7003ab2d4c6308
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2022-09-06 12:27:25 -0700

    feat: temperature display (wip)

--- cryptid-temperature.h
+++ cryptid-temperature.h
@@ -30,39 +30,9 @@
       gradient_config_t gradient_config;
 
       /**
-       * @brief Pixel masks for each digit.
-       */
-      pixel_mask_t temp_mask[3];
-
-      /**
-       * @brief The image structs for each digit.
-       */
-      gradient_image_t gradient_image[3];
-
-      /**
        * @brief The current temperature.
        */
       uint8_t temperature;
-
-      /**
-       * @brief The current temperature (in characters).
-       */
-      String temperatureChars = "000";
-
-      /**
-       * @brief Font width.
-       */
-      uint8_t font_width = 9;
-
-      /**
-       * @brief Font height.
-       */
-      uint8_t font_height = 9;
-
-      /**
-       * @brief Font width * font height.
-       */
-      int drawCharSize;
 
       /**
        * @brief x-coord to draw on pixels[] grid.
