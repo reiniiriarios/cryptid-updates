6f6ac3650cc03623a5a0877e24f046b5b34c7598
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2022-09-10 14:16:13 -0700

    style: header indent pt2

--- cryptid-temperature.h
+++ cryptid-temperature.h
@@ -1,53 +1,53 @@
 #ifndef H_CRYPTID_TEMP
 #define H_CRYPTID_TEMP
 
-  #include "cryptid-types.h"
-  #include "cryptid-gfx.h"
+#include "cryptid-types.h"
+#include "cryptid-gfx.h"
 
-  class TemperatureDisplay {
-    public:
-      /**
-       * @brief Construct a new TemperatureDisplay object
-       * 
-       * @param gfx_p A pointer to the graphics object.
-       */
-      TemperatureDisplay(Gfx *gfx_p);
+class TemperatureDisplay {
+  public:
+    /**
+     * @brief Construct a new TemperatureDisplay object
+     * 
+     * @param gfx_p A pointer to the graphics object.
+     */
+    TemperatureDisplay(Gfx *gfx_p);
 
-      /**
-       * @brief Update the temperature image (run each frame).
-       */
-      void update(float newTemperature);
+    /**
+     * @brief Update the temperature image (run each frame).
+     */
+    void update(float newTemperature);
 
-      /**
-       * @brief Convert temperature to hue by scaling the value to a different range.
-       */
-      uint16_t temperature2hue(int value, int minFrom, int maxFrom, int minTo, int maxTo);
+    /**
+     * @brief Convert temperature to hue by scaling the value to a different range.
+     */
+    uint16_t temperature2hue(int value, int minFrom, int maxFrom, int minTo, int maxTo);
 
-    private:
-      /**
-       * @brief A pointer to the graphics object.
-       */
-      Gfx* gfx;
+  private:
+    /**
+     * @brief A pointer to the graphics object.
+     */
+    Gfx* gfx;
 
-      /**
-       * @brief The gradient config struct.
-       */
-      gradient_config_t gradient_config;
+    /**
+     * @brief The gradient config struct.
+     */
+    gradient_config_t gradient_config;
 
-      /**
-       * @brief The current temperature.
-       */
-      uint8_t temperature;
+    /**
+     * @brief The current temperature.
+     */
+    uint8_t temperature;
 
-      /**
-       * @brief x-coord to draw on pixels[] grid.
-       */
-      uint8_t xStart;
+    /**
+     * @brief x-coord to draw on pixels[] grid.
+     */
+    uint8_t xStart;
 
-      /**
-       * @brief y-coord to draw on pixels[] grid.
-       */
-      uint8_t yStart;
-  };
+    /**
+     * @brief y-coord to draw on pixels[] grid.
+     */
+    uint8_t yStart;
+};
 
 #endif
